69991a413ed3ec4ab6014749c98ab2d9e52a5a2a
Daniel Kim <dkim6613@yahoo.com>
2021-08-25 16:04:47 -0700

    Completed Q3 - arraySum

--- src/recursion.js
+++ src/recursion.js
@@ -20,16 +20,29 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
-
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sum(array.slice(1)); //1 + (sum[2,3,4,5,6])
+                                           //1 + 2 + (sum[3,4,5,6])
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var stringArr = array.toString();
+  var splitArr = stringArr.split(",").map(Number);
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return splitArr[0] + arraySum(splitArr.slice(1));
+  }
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+
 };
 
 // 5. Sum all integers below a given integer.
